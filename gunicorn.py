90f2d4605ca112e3d6b6e6f07573e62bbc7f2d06
David Albert <davidbalbert@gmail.com>
2017-05-11 18:30:09 -0400

    Keep important threads alive

--- gunicorn.py
+++ gunicorn.py
@@ -1,21 +1,33 @@
 from threading import Thread
 import os
 import sys
+import traceback
 
 sys.path.insert(0, os.getcwd())
 
 from bot import run_bot
 from poller import run_poller
 
+def keep_alive(f):
+    def wrapped():
+        while True:
+            try:
+                f()
+            except BaseException as e:
+                #traceback.print_exc(*sys.exc_info())
+                print(traceback.format_exc())
+
+    return wrapped
+
 def when_ready(server):
     # TODO: daemon threads are stopped abruptly when the process is killed.
     # we need to do some sort of graceful shutdown to make sure that all
     # database transactions are flushed. Maybe by using Event:
     # (https://docs.python.org/3/library/threading.html#threading.Event)
-    bot = Thread(target=run_bot)
+    bot = Thread(target=keep_alive(run_bot))
     bot.daemon = True
     bot.start()
 
-    poller = Thread(target=run_poller)
+    poller = Thread(target=keep_alive(run_poller))
     poller.daemon = True
     poller.start()
